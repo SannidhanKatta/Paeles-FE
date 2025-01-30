91519d29546bbcf87a030411809e18e7d3bd20e1
Sai Shankar <saishankar15052005@gmail.com>
2025-01-30 12:11:43 +0530

    fix

--- src/pages/policies/TermsAndConditions.jsx
+++ src/pages/policies/TermsAndConditions.jsx
@@ -204,7 +204,7 @@ const TermsAndConditions = () => {
                 <div>
                     <h2 className="text-xl font-semibold mb-4">INDEMNIFICATION</h2>
                     <p className='mb-4'>
-                        You agree to indemnify, defend and hold harmless My Store and our parent, subsidiaries, affiliates, partners, officers, directors, agents, contractors, licensors, Service providers, subcontractors, suppliers, interns and employees, harmless from any claim or demand, including reasonable attorneys’ fees, made by any third-party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.
+                        You agree to indemnify, defend and hold harmless HANDY BASKET (OPC) PRIVATE LIMITED and our parent, subsidiaries, affiliates, partners, officers, directors, agents, contractors, licensors, Service providers, subcontractors, suppliers, interns and employees, harmless from any claim or demand, including reasonable attorneys’ fees, made by any third-party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.
                     </p>
                 </div>
 
